d30da9d812009eb18a0c437c5fc8f1824e8471ca
unclewo <38658421+unclewo@users.noreply.github.com>
2018-12-10 10:49:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -909,7 +909,7 @@ install_v2ray() {
 	mkdir -p /tmp/v2ray
 
 	v2ray_tmp_file="/tmp/v2ray/v2ray.zip"
-	v2ray_ver="$(curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/v2ray/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)"
+	v2ray_ver="$(curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/v2ray/v2ray-core/releases/latest | v4.8.0 | cut -d\" -f4)"
 
 	if [[ ! $v2ray_ver ]]; then
 		echo
